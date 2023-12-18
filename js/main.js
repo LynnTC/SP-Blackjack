75c184ca6510f1b3e22463a720c279e4cc770177
LynnTCai@gmail.com <LynnTCai@gmail.com>
2023-12-18 02:48:13 -0600

    adding cards and basic bet function (note: I am bad at listening)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,130 @@
-	/*----- constants -----*/
+/*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h']
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const originalDeck = buildOriginalDeck();
+renderDeckInContainer(originalDeck, document.getElementById('original-deck-container'));
 
+/*----- state variables -----*/
+let money;
+let pHand;
+let dHand;
+let winner;
+let shuffledDeck;
 
-	/*----- state variables -----*/
+/*----- cached elements  -----*/
+const shuffledContainer = document.getElementById('shuffled-deck-container');
 
+/*----- event listeners -----*/
+document.querySelector('body')
+    .addEventListener('click', handleBet);
 
-	/*----- cached elements  -----*/
+document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
 
+/*----- functions -----*/
 
-	/*----- event listeners -----*/
+init ();
 
+function init(){
+    pHand = {
+        cards:'',
+        value:0,
+        amountBet:0
+    };
+    dHand = {
+        cards:'',
+        value:0
+    };
+    winner = 't';
+    money = 5000;
+    render();
 
-	/*----- functions -----*/
-    init ();
+}
 
-    function init(){
-        
+function handleBet(evt){
+    if (evt.target.tagName !== 'BUTTON') return;
+    pHand.amountBet = parseInt(evt.target.innerText);
+    money -= pHand.amountBet;
+    renderBet();
+    hideBetButtons();
+    render();
+}
+
+function hideBetButtons(){
+    const betButtons = document.querySelectorAll('button[id^="bet"]');
+    betButtons.forEach(button =>{
+        button.style.display = 'none';
+    });
+}
+
+function showBetButtons(){
+    const betButtons = document.querySelectorAll('button[id^="bet"]');
+    betButtons.forEach(button =>{
+        button.style.display = '';
+    })
+}
+
+function renderMoney(){
+    const moneyEl = document.getElementById(`playMoney`);
+    moneyEl.innerText = money;
+}
+
+function renderBet(){
+    const betEl = document.getElementById(`betMoney`);
+    betEl.innerText = pHand.amountBet;
+}
+
+function render(){
+    renderMoney();
+}
+
+/*----- functions -----*/
+function getNewShuffledDeck() {
+    // Create a copy of the originalDeck (leave originalDeck untouched!)
+    const tempDeck = [...originalDeck];
+    const newShuffledDeck = [];
+    while (tempDeck.length) {
+      // Get a random index for a card still in the tempDeck
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
     }
+    return newShuffledDeck;
+  }
+  
+  function renderNewShuffledDeck() {
+    // Create a copy of the originalDeck (leave originalDeck untouched!)
+    shuffledDeck = getNewShuffledDeck();
+    renderDeckInContainer(shuffledDeck, shuffledContainer);
+  }
+  
+  function renderDeckInContainer(deck, container) {
+    container.innerHTML = '';
+    // Let's build the cards as a string of HTML
+    let cardsHtml = '';
+    deck.forEach(function(card) {
+      cardsHtml += `<div class="card ${card.face}"></div>`;
+    });
+    // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
+    // const cardsHtml = deck.reduce(function(html, card) {
+    //   return html + `<div class="card ${card.face}"></div>`;
+    // }, '');
+    container.innerHTML = cardsHtml;
+  }
+  
+  function buildOriginalDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+        deck.push({
+          // The 'face' property maps to the library's CSS classes for cards
+          face: `${suit}${rank}`,
+          // Setting the 'value' property for game of blackjack, not war
+          value: Number(rank) || (rank === 'A' ? 11 : 10)
+        });
+      });
+    });
+    return deck;
+  }
+  
+  renderNewShuffledDeck();
